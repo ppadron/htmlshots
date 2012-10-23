87bf185c0bd076f836a720c02713172c18f2cab8
Pedro Padron <ppadron@w3p.com.br>
2012-10-23 00:45:17 -0200

    there is no need for waitFor() anymore, since we are manually setting page.content now

--- htmlshots.js
+++ htmlshots.js
@@ -2,54 +2,6 @@ var port    = phantom.args[0] ? phantom.args[0] : 3000;
 var server  = require('webserver').create();
 var page    = require('webpage').create();
 
-/**
- * Waits for maxTimeOut until a certain condition is met.
- * 
- * @param  callback testFx     Callback that returns true/false.
- * @param  callback onReady    Callback to be executed when condition is met.
- * @param  callback onTimeout  Callback to be executed when timeout is reached.
- * @param  int      maxTimeOut Max timeout in milliseconds.
- * 
- * @return void
- */
-function waitFor(testFx, onReady, onTimeout, maxTimeOut) {
-    var maxTimeOut = maxTimeOut ? maxTimeOut : 3000;
-    var start      = new Date().getTime();
-    var condition  = false;
-    var check      = function () {
-        if ((new Date().getTime() - start < maxTimeOut) && !condition ) {
-            condition = (typeof(testFx) === "string" ? eval(testFx) : testFx());
-        } else {
-            if(!condition) {
-                typeof(onTimeout) === "string" ? eval(onTimeout) : onTimeout();
-                clearInterval(interval);
-            } else {
-                typeof(onReady) === "string" ? eval(onReady) : onReady();
-                clearInterval(interval);
-            }
-        }
-    };
-    interval = setInterval(check, 400);
-}
-
-/**
- * Checks if all images have been loaded.
- * 
- * @return boolean
- */
-function imagesLoaded() {
-    return page.evaluate(function() {
-        var images     = document.getElementsByTagName('img');
-        var isComplete = true;
-        for (var i = 0; i < images.length; i++) {
-            if (!images[i].complete) {
-                isComplete = false;
-            }
-        }
-        return isComplete;
-    });
-}
-
 var service = server.listen(port, function (request, response) {
 
     var send = function (httpStatus, message) {
@@ -77,11 +29,7 @@ var service = server.listen(port, function (request, response) {
         if (status != 'success') {
             send(500, {error: 'Sorry, something wrong happened.'});
         }
-        waitFor(imagesLoaded,
-            function () { return send(200, page.renderBase64()); },
-            function () { return send(500, {error: 'A timeout has ocurred.'}); },
-            10000
-        );
+        return send(200, page.renderBase64());
     };
     page.content = contents;
 });
